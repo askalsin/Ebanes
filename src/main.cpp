c83e6e2bfe7a86ac040691828accdc0ff8d1badf
Aleksey Kalsin <askalsin@ya.ru>
2023-10-28 09:50:12 +0300

    OpenGL and glfw initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,50 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <cstddef>
 #include <iostream>
+
+int window_size_X = 640;
+int window_size_Y = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height)
+{
+    window_size_X = width;
+    window_size_Y = height;
+    glViewport(0, 0, window_size_X, window_size_Y);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
 
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit()) return -1;
+    if (!glfwInit()) {
+        std::cout << "Library initialization failed!\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(window_size_X, window_size_Y, "Ebanes", nullptr,
+                              nullptr);
     if (!window) {
+        std::cout << "Failed to create a window!\n";
         glfwTerminate();
         return -1;
     }
+
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -24,7 +54,8 @@ int main(void)
         return -1;
     }
 
-    std::cout << "OpenCL: " << GLVersion.major << GLVersion.minor << "\n";
+    std::cout << "Render: " << glGetString(GL_RENDERER) << "\n";
+    std::cout << "OpenGL version: " << glad_glGetString(GL_VERSION) << "\n";
 
     glClearColor(0, 1, 0, 1);
 
